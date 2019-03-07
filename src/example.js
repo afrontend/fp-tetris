ce48e657944acd96a9a4a4f8743a795d9d5b71e9
afrontend <frontend.wordpress@gmail.com>
2019-03-07 22:49:29 +0900

    process arrow key

--- src/example.js
+++ src/example.js
@@ -1,28 +1,37 @@
 const clear = require('clear');
-const fpTetris = require('../lib/index.js')
-
+const fpTetris = require('../lib/index.js');
+const keypress = require('keypress');
 
 const global = {
   state: fpTetris.initTetrisTable()
 };
 
+keypress(process.stdin);
+
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit();
+  }
+  if (key && key.name == 'q') {
+    process.exit();
+  }
+  if (key) {
+    global.state = fpTetris.keyTetrisTable(key.name, global.state);
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
 const format = (ary) => {
   return ary.map((r) => (
-    r.map((item) => (item.color === 'grey'? '-' : 'X'))
+    r.map((item) => (item.color === 'grey'? ' ' : '■')).join()
   ))
 };
 
 global.timer = setInterval(() => {
-  global.state = (fpTetris.downTetrisTable(global.state));
+  global.state = fpTetris.downTetrisTable(global.state);
   clear();
-  console.log(format(fpTetris.tetrisTable(global.state)));
+  console.log(format(fpTetris.joinTetrisTable(global.state)));
 }, 700);
-
-  /*
-    * keyboard.keyPressed(e => {
-    *   setTimeout(() => {
-    *     this.setState((state) => (keyTetrisTable(e.which, state)));
-    *   });
-    * });
-    */
 
