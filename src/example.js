34adc9de1b8b3cb427f1c1ffc103628e4b0be21b
afrontend <frontend.wordpress@gmail.com>
2019-03-13 21:40:10 +0900

    add full size option

--- src/example.js
+++ src/example.js
@@ -1,38 +1,58 @@
 #!/usr/bin/env node
 const clear = require('clear');
-const fpTetris = require('../lib/index.js');
 const keypress = require('keypress');
+const program = require('commander');
+const fpTetris = require('../lib/index.js');
+const pkg = require('../package.json');
 
-const global = {
-  state: fpTetris.initTetrisTable(20, 30)
-};
+program
+  .version(pkg.version)
+  .option('-f, --full', 'terminal full size')
+  .parse(process.argv);
 
-keypress(process.stdin);
+const startGame = (rows = 17, columns = 15) => {
+  const global = {
+    state: fpTetris.initTetrisTable(rows, columns)
+  };
 
-process.stdin.on('keypress', function (ch, key) {
-  if (key && key.ctrl && key.name === 'c') {
-    process.exit();
+  keypress(process.stdin);
+
+  process.stdin.on('keypress', function (ch, key) {
+    if (key && key.ctrl && key.name === 'c') {
+      process.exit();
+    }
+    if (key && key.name === 'q') {
+      process.exit();
+    }
+    if (key) {
+      global.state = fpTetris.keyTetrisTable(key.name, global.state);
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+
+  const format = (ary) => {
+    return ary.map((r) => (
+      r.map((item) => (item.color === 'grey'? ' ' : '■')).join(' ')
+    ))
+  };
+
+  global.timer = setInterval(() => {
+    global.state = fpTetris.downTetrisTable(global.state);
+    clear();
+    console.log(format(fpTetris.joinTetrisTable(global.state)));
+  }, 400);
+}
+
+const activate = (program) => {
+  console.log(program.full);
+  if (program.full) {
+    startGame(process.stdout.rows - 2, (process.stdout.columns / 2) - 4 );
+  } else {
+    startGame();
   }
-  if (key && key.name === 'q') {
-    process.exit();
-  }
-  if (key) {
-    global.state = fpTetris.keyTetrisTable(key.name, global.state);
-  }
-});
+}
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
-
-const format = (ary) => {
-  return ary.map((r) => (
-    r.map((item) => (item.color === 'grey'? ' ' : '■')).join(' ')
-  ))
-};
-
-global.timer = setInterval(() => {
-  global.state = fpTetris.downTetrisTable(global.state);
-  clear();
-  console.log(format(fpTetris.joinTetrisTable(global.state)));
-}, 400);
+activate(program);
 
